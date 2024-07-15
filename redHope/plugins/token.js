f56f98d05ba2e90dce9eaed27b8cbbf11826a2fc
Rajamiththiran <rjmithu7@gmail.com>
2024-07-15 14:13:49 +0530

    token plugin created

---
+++ redHope/plugins/token.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const fp = require("fastify-plugin");
+const moment = require("moment");
+const { v4: uuidv4 } = require("uuid");
+
+module.exports = fp(async function (fastify, opts) {
+  const token = {
+    create: async (params = {}) => {
+      //email
+      const key = uuidv4();
+      const query = await fastify.prisma.user_tokens.create({
+        data: {
+          email: params.email,
+          token: key,
+          created_at: moment().toISOString(),
+          updated_at: moment().toISOString(),
+        },
+      });
+      return query.token;
+    },
+
+    delete: async (params = {}) => {
+      //token
+      const query = await fastify.prisma.user_tokens.delete({
+        where: {
+          tokens: params.token,
+        },
+      });
+      return query;
+    },
+  };
+  fastify.decorate("token", token);
+});

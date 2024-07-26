1cbac76d016464830e0b6e426be6997666f14b2d
Rajamiththiran <rjmithu7@gmail.com>
2024-07-27 00:05:18 +0530

    knowledges migration file created

---
+++ redHope/migrations/20240726182235-add-knowledges.js
@@ -0,0 +1,74 @@
+"use strict";
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db) {
+  return db.createTable("knowledges", {
+    id: {
+      type: "int",
+      unsigned: true,
+      notNull: true,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    hospital_id: {
+      type: "int",
+      unsigned: true,
+      notNull: true,
+      foreignKey: {
+        name: "knowledges_hospital_id_fk",
+        table: "hospitals",
+        mapping: "id",
+        rules: {
+          onDelete: "CASCADE",
+          onUpdate: "RESTRICT",
+        },
+      },
+    },
+    knowledge: {
+      type: "text",
+      notNull: true,
+    },
+    image_url: {
+      type: "string",
+      null: true,
+    },
+    created_at: {
+      type: "timestamp",
+      timezone: true,
+      notNull: true,
+      defaultValue: new String("CURRENT_TIMESTAMP"),
+    },
+    modified_at: {
+      type: "timestamp",
+      timezone: true,
+      notNull: true,
+      defaultValue: new String("CURRENT_TIMESTAMP"),
+    },
+    deleted_at: {
+      type: "timestamp",
+      timezone: true,
+      null: true,
+    },
+  });
+};
+
+exports.down = function (db) {
+  return db.dropTable("knowledges");
+};
+
+exports._meta = {
+  version: 1,
+};

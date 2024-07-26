bba75755ef9ff46ebe0e100f9c90225133983c13
Rajamiththiran <rjmithu7@gmail.com>
2024-07-27 00:05:00 +0530

    thoughts migration file created

--- redHope/migrations/20240724131012-add-thoughts.js
+++ redHope/migrations/20240724131012-add-thoughts.js
@@ -1,27 +1,74 @@
-'use strict';
+"use strict";
 
 var dbm;
 var type;
 var seed;
 
 /**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
-exports.setup = function(options, seedLink) {
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
   dbm = options.dbmigrate;
   type = dbm.dataType;
   seed = seedLink;
 };
 
-exports.up = function(db) {
-  return null;
+exports.up = function (db) {
+  return db.createTable("thoughts", {
+    id: {
+      type: "int",
+      unsigned: true,
+      notNull: true,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    donor_id: {
+      type: "int",
+      unsigned: true,
+      notNull: true,
+      foreignKey: {
+        name: "thoughts_donor_id_fk",
+        table: "donors",
+        mapping: "id",
+        rules: {
+          onDelete: "CASCADE",
+          onUpdate: "RESTRICT",
+        },
+      },
+    },
+    title: {
+      type: "string",
+      notNull: true,
+    },
+    thought: {
+      type: "text",
+      notNull: true,
+    },
+    created_at: {
+      type: "timestamp",
+      timezone: true,
+      notNull: true,
+      defaultValue: new String("CURRENT_TIMESTAMP"),
+    },
+    modified_at: {
+      type: "timestamp",
+      timezone: true,
+      notNull: true,
+      defaultValue: new String("CURRENT_TIMESTAMP"),
+    },
+    deleted_at: {
+      type: "timestamp",
+      timezone: true,
+      null: true,
+    },
+  });
 };
 
-exports.down = function(db) {
-  return null;
+exports.down = function (db) {
+  return db.dropTable("thoughts");
 };
 
 exports._meta = {
-  "version": 1
+  version: 1,
 };
